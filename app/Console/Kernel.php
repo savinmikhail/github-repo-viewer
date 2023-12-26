2d7f4c0c792ced0a64aea31576f59c66d2fcce43
Mikhail <mikhail.d.savin@gmail.com>
2023-12-26 23:13:17 +0700

    fix(command): running interval

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('fetch:repositories')->everyTenSeconds();
+        $schedule->command('fetch:repositories')->everyTenMinutes();
     }
 
     /**
